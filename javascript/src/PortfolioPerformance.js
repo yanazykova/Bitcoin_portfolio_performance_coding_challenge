7bcc0342f7365af1bd7ddfa99e23cfc909a2e5ed
yanazykova <yvz1973@gmail.com>
2021-10-06 13:38:51 +0100

    some addition to the pseudocode

--- javascript/src/PortfolioPerformance.js
+++ javascript/src/PortfolioPerformance.js
@@ -17,26 +17,53 @@ const transactions = [
 
 // Complete this method to return a list of daily portfolio values with one record for each day from the 01-09-2021-07-09-2021 in ascending date order
 
-export function getDailyPortfolioValues() {
+function getDailyPortfolioValues(prices, transactions) {
 
-    let customerWallet = 0
-    let performanceValue = 0
-    let dailyPerformanceValueList = []
+    
+    
+    // Plan A:
+    // 1. loop through the prices list:
 
-    // loop through the transactions list
-        // loop through the price list
-        // if the date of the price == date of the transaction
-        // then add the value of the transaction to the client's bitcoins wallet
-        // then multiply clients bitcoins by price = performance value
-        // assign performanceValue
+        //2. compare the dates within the array (use setHours method to exclude hours and minutes?) and identify objects with equal dates (if there is >1 date with the same value):             
+
+        //3. pick date containing the most recent fund price
+        // and assign the result to the variable dailyPrice
+
+    // 4. loop through the transactions list
+        // identify keys with equal date values  
+        // add together all the values for this date 
+        // assign the result to variable dailyBitcoins
+    
+    // 5. multiply dailyPrice by dailyBitcoins
+    // assign the result to dailyPerformanceValueList
+
+    
+    // Plan B:
+
+    // let customerBitcoins = 0
+    // let performanceValue = 0
+    // let dailyPerformanceValueList = []
+
+    
+        // if the date in the price list == date in the transaction list:
+        // add the value of the transaction to the customerBitcoins;
+
+        // for (object in transactions)
+        //  if (transactions[effectiveDate] ==  prices[effectiveDate])
+        //  customerBitcoins.push(transactions[value])
+
+        // then multiply clients bitcoins by price (= performance value)
+        // assign the result to performanceValue
         // add new element to dailyPerformanceValueList with date and performance value
-        // if not has the same date
-        // then add new element to dailyPerformanceValueList with the date amd the performance value
+        // if it does not have the same date:
+        // add new element to dailyPerformanceValueList with the date and the performance value
         // 
-        // if the date in the date list !== the date of the transaction list
+        // if the date in the price list !== the date of the transaction list
         // then customerWallet * price = performanceValue
         // assign performanceValue
         // add new element to dailyPerformanceValueList with the date and the performance value
-    // return dailyPerformanceValueList
+    
     return [];
 }
+
+getDailyPortfolioValues(prices, transactions)
